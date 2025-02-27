c560b74f901e0e834ae51b0dbcb1cff7b97d7c1c
unknown <maksimus247@gmail.com>
2025-02-27 19:41:22 +0200

    1

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import tkinter as tk
 from datetime import datetime
-from bs4 import BeautifulSoup
 import calendar
 import requests
-import cairosvg
-from PIL import Image, ImageTk
-from io import BytesIO
 
 
 def update_time():
@@ -17,82 +13,72 @@ def update_time():
     date_label.config(text=current_date)
     weekday_label.config(text=current_weekday[:3])
     
+    if int(current_time[:2]) > 22:
+        to_dark_mode(True)
+    else:
+        to_dark_mode(False)
+    
     root.after(1000, update_time)
     
-
+    
 def update_weather():
     try:
         weather = requests.get(url="https://wttr.in/kyiv", params="format=1").text
     except:
         weather = "❌"
     
-    w_url = "https://www.wunderground.com/hourly/ua/kyiv"
-    try:
-        markup = requests.get(w_url).text
-        soup = BeautifulSoup(markup, features="html.parser")
-        res = soup.find(name="div", class_="small-2 columns fct-icon").findChild("img")["src"]
-        img_url = f"http://{res [6:]}" 
-        png_image = cairosvg.svg2png(url=img_url)
-        png_bytes = BytesIO(png_image)
-        image = Image.open(png_bytes)
-        tk_image = ImageTk.PhotoImage(image)
-        weather_img_label.config(image=tk_image)
-    except:
-        weather_img_label.config(text="X")
-    
-    res = f"{weather[5:-2]}"
+    res = f"\n{weather[5:]}"
     weather_label.config(text=res)
     root.after(3600000, update_weather)
     
-
+    
+def to_dark_mode(go_dark:bool):
+    if go_dark:
+        root.config(background="#404040")
+        time_label.config(background="#404040")
+        
+        frame.config(background="#404040")
+        date_label.config(background="#404040")
+        weekday_label.config(background="#404040")
+        weather_label.config(background="#404040")
+    else:
+        root.config(background="white")
+        time_label.config(background="white")
+        
+        frame.config(background="white")
+        date_label.config(background="white")
+        weekday_label.config(background="white")
+        weather_label.config(background="white")
+        
+        
+    
 root = tk.Tk()
-
-# def update_weather_img():
-#     w_url = "https://www.wunderground.com/hourly/ua/kyiv"
-#     try:
-#         markup = requests.get(w_url).text
-#         soup = BeautifulSoup(markup, features="html.parser")
-#         res = soup.find(name="div", class_="small-2 columns fct-icon").findChild("img")["src"]
-#         img_url = f"http://{res [6:]}" 
-#         png_image = cairosvg.svg2png(url=img_url)
-#         png_bytes = BytesIO(png_image)
-#         image = Image.open(png_bytes)
-#         tk_image = ImageTk.PhotoImage(image)
-#         weather_img_label.config(image=tk_image)
-#     except:
-#         weather_img_label.config(text="X")
-#     root.after(3600000, update_weather_img)
-    
-    
-root.title("Часы")
+root.title("Clock")
 root.geometry("1000x550")
 root.resizable(True, True)
 
 font_time = ("Helvetica", 190, "bold") 
 font_date = ("Helvetica", 80)
-font_weather = ("Helvetica", 80)
+font_weather = ("Helvetica", 60)
+
 
 time_label = tk.Label(root, font=font_time, fg="black")
 time_label.pack()
 
+# ===================== BOTTOM THINGS ========================
 frame = tk.Frame(root)
-frame.pack(pady=30)
+frame.pack()
 
-date_label = tk.Label(frame, font=font_date, fg="#3b3b3b")
+date_label = tk.Label(frame, font=font_date, fg="black")
 date_label.pack(side=tk.LEFT)
 
-weekday_label = tk.Label(frame, font=font_date, fg="#3b3b3b")
-weekday_label.pack(side=tk.LEFT, padx=40)
+weekday_label = tk.Label(frame, font=font_date, fg="black")
+weekday_label.pack(side=tk.LEFT, padx=80)
 
-weather_img_label = tk.Label(frame, image=None)
-weather_img_label.pack(side=tk.LEFT)
-
-weather_label = tk.Label(frame, font=font_weather, fg="#3b3b3b")
-weather_label.pack(side=tk.LEFT)
+weather_label = tk.Label(frame, font=font_weather, fg="black")
+weather_label.pack(side=tk.LEFT, padx=20)
 
 update_time()
 update_weather()
-# update_weather_img()
 
 root.mainloop()
-
