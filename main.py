7648970ec5ac52d4c6bfaa1d05d321e6d7a578c0
unknown <maksimus247@gmail.com>
2025-02-27 19:08:31 +0200

    make a dark mode

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import calendar
 import requests
 
+
 def update_time():
     current_time = datetime.now().strftime("%H:%M:%S")
     current_date = datetime.now().strftime("%d/%m")
@@ -14,7 +15,13 @@ def update_time():
     date_label.config(text=current_date)
     weekday_label.config(text=current_weekday[:3])
     
+    if int(current_time[:2]) > 22:
+        to_dark_mode(True)
+    else:
+        to_dark_mode(False)
+    
     root.after(1000, update_time)
+    
     
 def update_weather():
     try:
@@ -22,17 +29,35 @@ def update_weather():
     except:
         weather = "❌"
     
-    res = f"{weather[0]}\n{weather[5:]}"
+    res = f"\n{weather[5:]}"
     weather_label.config(text=res)
     root.after(3600000, update_weather)
     
-# def to_dark_mode():
-
+    
+def to_dark_mode(go_dark:bool):
+    if go_dark:
+        root.config(background="#404040")
+        time_label.config(background="#404040")
+        
+        frame.config(background="#404040")
+        date_label.config(background="#404040")
+        weekday_label.config(background="#404040")
+        weather_label.config(background="#404040")
+    else:
+        root.config(background="white")
+        time_label.config(background="white")
+        
+        frame.config(background="white")
+        date_label.config(background="white")
+        weekday_label.config(background="white")
+        weather_label.config(background="white")
+        
+        
+    
 root = tk.Tk()
 root.title("Часы")
 root.geometry("1000x550")
 root.resizable(True, True)
-root.config(background="#404040")
 
 font_time = ("Helvetica", 190, "bold") 
 font_date = ("Helvetica", 80)
@@ -41,24 +66,19 @@ font_weather = ("Helvetica", 60)
 
 time_label = tk.Label(root, font=font_time, fg="black")
 time_label.pack()
-time_label.config(background="#404040")
 
 # ===================== BOTTOM THINGS ========================
 frame = tk.Frame(root)
 frame.pack()
-frame.config(background="#404040")
 
 date_label = tk.Label(frame, font=font_date, fg="black")
 date_label.pack(side=tk.LEFT)
-date_label.config(background="#404040", highlightcolor="#404040")
 
 weekday_label = tk.Label(frame, font=font_date, fg="black")
 weekday_label.pack(side=tk.LEFT, padx=80)
-weekday_label.config(background="#404040")
 
 weather_label = tk.Label(frame, font=font_weather, fg="black")
 weather_label.pack(side=tk.LEFT, padx=20)
-weather_label.config(background="#404040")
 
 update_time()
 update_weather()
